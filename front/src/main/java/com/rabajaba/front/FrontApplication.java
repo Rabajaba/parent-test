ff3193d6ba1bfffeb872f61293b585565b8ea187
Kirill Velikanov <kavel@ukr.net>
2018-10-25 13:20:59 +0300

    finished zuul config

--- front/src/main/java/com/rabajaba/front/FrontApplication.java
+++ front/src/main/java/com/rabajaba/front/FrontApplication.java
@@ -5,12 +5,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @Slf4j
+@RestController
 public class FrontApplication {
     public static void main(String[] args) {
         SpringApplication.run(FrontApplication.class, args);
@@ -21,5 +26,10 @@ public class FrontApplication {
     @PostConstruct
     public void initialize() {
         log.info("Hello from yollo: {}.", yolloProp);
+    }
+
+    @RequestMapping("/")
+    public String test() {
+        return "Yollo";
     }
 }
--- zuul/src/main/java/com/rabajaba/zuul/ZuulConfig.java
+++ zuul/src/main/java/com/rabajaba/zuul/ZuulConfig.java
@@ -1,26 +1,29 @@
-package com.rabajaba.discovery;
+package com.rabajaba.zuul;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 import javax.annotation.PostConstruct;
 
+@EnableZuulProxy
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 @Slf4j
-public class DiscoveryServer {
+public class ZuulConfig {
     @Value("${server.port}")
-    private int port;
+    private Integer port;
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServer.class, args);
+        SpringApplication.run(ZuulConfig.class, args);
     }
 
     @PostConstruct
     public void initialize() {
-        log.info("Hello from Eureka Server on port {}.", port);
+        log.info("Hello from zuul on port {}.", port);
     }
 }
