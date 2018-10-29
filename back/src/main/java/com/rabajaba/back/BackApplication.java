68d8e02ecdb4cbe8d595a040a963cc49edfb38cd
Kirill Velikanov <kavel@ukr.net>
2018-10-29 15:50:38 +0200

    made front app to use back through load balancer

--- back/src/main/java/com/rabajaba/back/BackApplication.java
+++ back/src/main/java/com/rabajaba/back/BackApplication.java
@@ -1,34 +1,34 @@
-package com.rabajaba.discovery;
+package com.rabajaba.back;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 @Slf4j
 @RestController
-public class DiscoveryServer {
-    @Value("${server.port}")
-    private int port;
-
+public class BackApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServer.class, args);
+        SpringApplication.run(BackApplication.class, args);
     }
+    @Value("yollo")
+    private String yolloProp;
 
     @PostConstruct
     public void initialize() {
-        log.info("Hello from Eureka Server on port {}.", port);
+        log.info("Hello from yollo: {}.", yolloProp);
     }
 
     @RequestMapping("/")
-    public String hello() {
-        return "Hello!";
+    public String test() {
+        return LocalDateTime.now().toString();
     }
 }
--- front/src/main/java/com/rabajaba/front/FrontApplication.java
+++ front/src/main/java/com/rabajaba/front/FrontApplication.java
@@ -1,35 +1,58 @@
 package com.rabajaba.front;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
 @Slf4j
 @RestController
 public class FrontApplication {
+    @Value("yollo")
+    private String yolloProp;
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     public static void main(String[] args) {
         SpringApplication.run(FrontApplication.class, args);
     }
-    @Value("yollo")
-    private String yolloProp;
 
     @PostConstruct
     public void initialize() {
         log.info("Hello from yollo: {}.", yolloProp);
     }
 
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder builder() {
+        return WebClient.builder();
+    }
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
     @RequestMapping("/")
     public String test() {
-        return "Yollo";
+//         return webClientBuilder.build().get().uri("http://back/back").retrieve().toString();
+        return "Response from back: " + restTemplate.getForObject("http://back", String.class);
     }
 }
